2f702970590aaf63e6709fdc46b2d7fa96218f7b
ravindugm <ravindumiyuranga@gmail.com>
2023-12-13 21:44:15 +0530

    Initial commit

---
+++ src/main/java/com/rmdev/contactmanagercore/ContactManagerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.rmdev.contactmanagercore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmdev/contactmanagercore/ContactManagerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmdev.contactmanagercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
